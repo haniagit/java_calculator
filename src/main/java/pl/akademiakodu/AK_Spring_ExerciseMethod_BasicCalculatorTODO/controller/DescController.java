860a65fc0f7d70e1e5477e10d6552037c7f593aa
Hania <haniagit08@gmail.com>
2018-07-09 13:22:00 +0200

    duzy lotek functionality

--- src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
@@ -5,18 +5,39 @@ package pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.controller;
  */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 @Controller
 public class DescController {
 
     /**
      * Method responsible to get description template
+     *
      * @return template with image containing description of exercise
      */
     @GetMapping("/desc")
-    public String descGet(){
+    public String descGet() {
         return "desc";
     }
 
+//    @ResponseBody
+    @GetMapping("/random")
+    public String getRandom(ModelMap map) {
+        Set<Integer> lotto = new TreeSet<>();
+        Random random = new Random();
+
+        while(lotto.size() !=6){
+            lotto.add(random.nextInt(49)+1);
+        }
+        map.put("lotto", lotto);
+
+        return "random";
+        //szuka pliku random.html, ktory musi znajdowac sie w katalogu templates
+    }
 }
