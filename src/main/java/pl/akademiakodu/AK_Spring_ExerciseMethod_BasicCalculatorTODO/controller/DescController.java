fde7b59bd071e187d194b32000881b002ef56a8a
Hania <haniagit08@gmail.com>
2018-07-10 15:28:29 +0200

    added new form

--- src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
@@ -7,7 +7,9 @@ package pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.models.Person;
 
 import java.util.Random;
 import java.util.Set;
@@ -26,18 +28,35 @@ public class DescController {
         return "desc";
     }
 
-//    @ResponseBody
+    //    @ResponseBody
     @GetMapping("/random")
     public String getRandom(ModelMap map) {
         Set<Integer> lotto = new TreeSet<>();
         Random random = new Random();
 
-        while(lotto.size() !=6){
-            lotto.add(random.nextInt(49)+1);
+        while (lotto.size() != 6) {
+            lotto.add(random.nextInt(49) + 1);
         }
         map.put("lotto", lotto);
 
         return "random";
         //szuka pliku random.html, ktory musi znajdowac sie w katalogu templates
     }
+
+    @GetMapping("/form")
+    public String getForm() {
+        return "form";
+    }
+
+    @GetMapping("/result")
+    public String result(@RequestParam
+                                 String firstName,
+                         @RequestParam String lastName,
+                         ModelMap modelMap) {
+
+        Person newPerson = new Person(firstName, lastName);
+        modelMap.put("person",newPerson);
+        return "person";
+    }
 }
+
